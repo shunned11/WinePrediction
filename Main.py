08b13d43574e6fa2323343c614a81974a562b816
anush <anushshand11@gmail.com>
2020-05-11 03:31:24 +0530

    Images Added

--- Main.py
+++ Main.py
@@ -1,19 +1,20 @@
 import tensorflow as tf
 import pandas as pd
 import numpy as np
-from Model import le
+# from Model import le
 from tensorflow.keras.models import model_from_json
 
-json_file = open('model.json', 'r')
+json_file = open('Model/model.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 loaded_model = model_from_json(loaded_model_json)
-loaded_model.load_weights("model.h5")
+loaded_model.load_weights("Model/model.h5")
 
-X_test=np.load("X_test.npy")
+print(loaded_model.summary())
+# X_test=np.load("X_test.npy")
 
-Y_test=loaded_model.predict_classes(X_test)
-Y_test=le.inverse_transform(Y_test)
-df=pd.read_csv("test.csv")
-df["variety"]=Y_test
-df.to_csv("Updated Test.csv",index=False)
+# Y_test=loaded_model.predict_classes(X_test)
+# Y_test=le.inverse_transform(Y_test)
+# df=pd.read_csv("test.csv")
+# df["variety"]=Y_test
+# df.to_csv("Updated Test.csv",index=False)
